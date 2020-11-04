8a3b8ec986e8c4e328fbe86511a8c9082e6f0e6a
Сидорова Ирина <isidorova@softrust.ru>
2020-11-05 00:24:32 +0300

    Реализована функция сравнения строк.

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,13 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+  const result = arr.slice();
+  result.sort((a, b) =>
+    a.localeCompare(b, ['ru-RU', 'en-En'],{caseFirst: 'upper'}));
 
+  if(param == 'asc') {
+    return result;
+  }
+
+  return result.reverse();
 }
