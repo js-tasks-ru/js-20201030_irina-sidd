219b3049f31f8b8cbeef65a9342152c44d0a14eb
Сидорова Ирина <isidorova@softrust.ru>
2020-11-23 17:30:33 +0300

    Реализован компонент toolTip.

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,5 +1,57 @@
 class Tooltip {
+  static instance;
 
+  onPointerOver = event => {
+    if(event.target.dataset.tooltip) {
+      this.render(event.target.dataset.tooltip);
+      this.toolTipMove(event);
+      document.addEventListener('pointermove', this.toolTipMove);
+    }
+  }
+
+  toolTipMove = event => {
+    this.element.style.left = `${event.clientX + 10}px`;
+    this.element.style.top = `${event.clientY + 10}px`;
+  }
+
+  onPointerOut = event => {
+    document.removeEventListener('pointermove', this.toolTipMove);
+    this.remove();
+  }
+
+  constructor() {
+    if(Tooltip.instance) {
+      return;
+    }
+
+    Tooltip.instance = this;
+  }
+
+  render(text) {
+    this.element = document.createElement('div');
+    this.element.className = 'tooltip';
+    this.element.innerHTML = text;
+
+    document.body.append(this.element);
+  }
+
+  initialize() {
+    document.addEventListener('pointerover', this.onPointerOver);
+    document.addEventListener('pointerout', this.onPointerOut);
+  }
+
+  remove() {
+    if(this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    document.removeEventListener('pointerover', this.onPointerOver);
+    document.removeEventListener('pointerout', this.onPointerOut);
+
+    this.remove();
+  }
 }
 
 const tooltip = new Tooltip();
